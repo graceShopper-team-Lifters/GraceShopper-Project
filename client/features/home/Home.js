65682a1336967a9462a1590853ae7df51879a25a
caseyjones90 <125698042+caseyjones90@users.noreply.github.com>
2023-06-15 13:05:58 -0400

    Updated Home and Navbar to access products page/Added Checkout page and clearCart function

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 /**
  * COMPONENT
  */
 const Home = (props) => {
   const username = useSelector((state) => state.auth.me.username);
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
 
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      <h3>Welcome, {isLoggedIn ? `${username}` : 'Guest' }</h3>
+      {isLoggedIn && (
+         <p>
+            You are logged in! Click <Link to='/products'>here</Link> to view our products.
+         </p>
+      )}
     </div>
   );
 };
