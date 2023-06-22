d33672fcc7d0723b347b030c447f1e578aa79fe2
resendezgabe <resendez.gabe@yahoo.com>
2023-06-21 23:18:34 -0400

    moved payment methods to checkoutpage

--- client/features/cart/CheckoutPage.js
+++ client/features/cart/CheckoutPage.js
@@ -4,57 +4,84 @@ import { Link, useNavigate } from "react-router-dom";
 import { clearCart } from "./cartSlice";
 
 const CheckoutPage = () => {
-   const cartItems = useSelector((state) => state.cart);
-   const dispatch = useDispatch();
-   const navigate = useNavigate();
+  const cartItems = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-   const [orderPlaced, setOrderPlaced] = useState(false);
+  const [paymentDetails, setPaymentDetails] = useState({
+    amount: "",
+    currency: "",
+    source: "",
+  });
 
-   const handleCheckout = async () => {
-      // Simulate checkout process
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+  const handleCheckout = async (e) => {
+    e.preventDefault();
+    // Simulate checkout process
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
-      dispatch(clearCart());
+    // Perform payment logic using paymentDetails state
+    console.log("Payment details:", paymentDetails);
 
-      setOrderPlaced(true);
-   };
+    dispatch(clearCart());
 
-   if (orderPlaced) {
-      return (
-         <div>
-            <h2>Order Placed</h2>
-            <p>Thank you for your purchase!</p>
-            <p>
-               <Link to='/products'>Continue Shopping</Link>
-            </p>
-         </div>
-      );
-   }
+    navigate("/order-placed");
+  };
 
-   return (
-      <div>
-         <h2>Checkout</h2>
-         {cartItems.length === 0 ? (
-            <p>Your cart is empty.</p>
-         ) : (
-            <>
-               <h3>Order Summary</h3>
-               <ul>
-                  {cartItems.map((item) => (
-                     <li key={item.id}>
-                        <p>{item.name}</p>
-                        <p>Quantity: {item.quantity}</p>
-                     </li>
-                  ))}
-               </ul>
-               <button onClick={handleCheckout}>Place Order</button>
-            </>
-         )}
-         <p>
-            <Link to='/cart'>Go back to Cart</Link>
-         </p>
-      </div>
-   );
+  const handlePaymentDetailsChange = (e) => {
+    const { id, value } = e.target;
+    setPaymentDetails((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  return (
+    <div>
+      <h2>Checkout</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <>
+          <h3>Order Summary</h3>
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id}>
+                <p>{item.name}</p>
+                <p>Quantity: {item.quantity}</p>
+              </li>
+            ))}
+          </ul>
+          <form onSubmit={handleCheckout}>
+            <input
+              type="text"
+              id="amount"
+              placeholder="Amount"
+              value={paymentDetails.amount}
+              onChange={handlePaymentDetailsChange}
+            />
+            <input
+              type="text"
+              id="currency"
+              placeholder="Currency"
+              value={paymentDetails.currency}
+              onChange={handlePaymentDetailsChange}
+            />
+            <input
+              type="text"
+              id="source"
+              placeholder="Source Token"
+              value={paymentDetails.source}
+              onChange={handlePaymentDetailsChange}
+            />
+            <button type="submit">Place Order</button>
+          </form>
+        </>
+      )}
+      <p>
+        <Link to="/cart">Go back to Cart</Link>
+      </p>
+    </div>
+  );
 };
 
 export default CheckoutPage;
